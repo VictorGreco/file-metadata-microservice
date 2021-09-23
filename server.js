df57506444bbaf4905aa656fbe7def25d7f1ca85
VictorGreco <victorgrecotech@gmail.com>
2021-09-23 07:36:12 +0200

    feature: added file metadata microservice

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
 
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const { json, urlencoded } = require('body-parser');
+const multer  = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
+const app = express();
 
 app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+  try {
+    const { originalname, mimetype, size } = req.file;
 
+    res.json({ name: originalname, type: mimetype, size });
+  } catch (err) {
+    res.send(err.message)
+  }
+})
 
 
 const port = process.env.PORT || 3000;
